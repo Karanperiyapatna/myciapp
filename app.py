d926f9e418130bbbaea2d91b2fe2353b10432852
Karanperiyapatna <periyapatnakaran@gmail.com>
2025-07-02 20:07:28 +0530

    @ 20:07

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from CI/CD to EC2!, updated CD/CI pipeline. Revoked at 19:52"
+    return "Hello from CI/CD to EC2!, updated CD/CI pipeline. Revoked at 20:07"
 
 if __name__ == "__main__":
     app.run()
